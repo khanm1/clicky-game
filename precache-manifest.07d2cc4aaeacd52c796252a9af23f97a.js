0489deaf830ea5a978d2e7f9ff25553ac91c90fc
mkhan <riponk9519@gamil.com>
2019-03-09 22:48:17 -0600

    Updates

---
+++ precache-manifest.07d2cc4aaeacd52c796252a9af23f97a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.895984e4.js"
+  },
+  {
+    "revision": "a94f94bd2eaaba4dc99f",
+    "url": "/clicky-game/static/js/main.40a09b70.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/clicky-game/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "a94f94bd2eaaba4dc99f",
+    "url": "/clicky-game/static/css/main.a0d89a3d.chunk.css"
+  },
+  {
+    "revision": "a6ac8636aa3720d5a7b5d60c68e2b578",
+    "url": "/clicky-game/index.html"
+  }
+];
